85d1c117db1bee336cbd3c6c1f998a68d527a313
Sean Sullivan <dev@seanmedia.ca>
2020-04-14 01:19:57 +0000

    Include body-parser as a dependency

--- express_server.js
+++ express_server.js
@@ -1,14 +1,22 @@
+// express_server.js
+
 const express = require('express');
-const app = express();
+const bodyParser = require("body-parser");
+
 const PORT = 8080;
 
+const app = express();
+
+// MIDDLEWARE
 app.set("view engine", "ejs");
+app.use(bodyParser.urlenindexd({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 }
 
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +24,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -30,6 +47,17 @@ app.get("/hello", (req, res) => {
   res.send("hello_world", templateVars);
 });
 
+// LISTENER
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  const alphaChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * alphaChars.length);
+    randomString += alphaChars[index]
+  }
+  return randomString;
+};
